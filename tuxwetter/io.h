25c0627108570d6645a6b810aad2dbd25b599380
Markus Volk <f_l_k@t-online.de>
2017-10-02 14:23:00 +0200

    import tuxwetter from bitbucket ni

---
+++ tuxwetter/io.h
@@ -0,0 +1,13 @@
+#ifndef __IO_H__
+
+#define __IO_H__
+
+#define RC_DEVICE	"/dev/input/event1"
+
+int InitRC(void);
+int CloseRC(void);
+int RCKeyPressed(void);
+int GetRCCode(int);
+void ClearRC(void);
+
+#endif
